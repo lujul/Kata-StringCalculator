c5f7def52521166909e8813ca114c467eead6b82
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-10 14:41:03 +0100

    Manage: sum of more than 2 values in the string

--- src/main/java/com/humanbooster/formation/testProject/StringCalculator.java
+++ src/main/java/com/humanbooster/formation/testProject/StringCalculator.java
@@ -1,25 +1,25 @@
 package com.humanbooster.formation.testProject;
 
 public class StringCalculator {
-	
+
 	/**
 	 * 
-	 * @param string, string containing integers to be summed
+	 * @param string,
+	 *            string containing integers to be summed
 	 * @return
 	 */
-	public static int add (String string) {
+	public static int add(String string) {
 		// return 0 if the string is empty
-		if(string.isEmpty()){
+		if (string.isEmpty()) {
 			return 0;
-		}else if (!string.contains(",")){
-			// return the single integer of the string
-			return Integer.valueOf(string);
-		}else {
-			// return the sum of the 2 intergers in the string
-			String [] valuesInString = string.split(",");
-			return Integer.valueOf(valuesInString[0])+ Integer.valueOf(valuesInString[1]);
+		} else {
+			// sum the int values in the string
+			int sum = 0;
+			String[] valuesInString = string.split(",");
+			for (String value : valuesInString) {
+				sum += Integer.parseInt(value);
+			}
+			return sum;
 		}
-			
 	}
-
 }
--- src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
+++ src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
@@ -21,10 +21,16 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-	// Test if the method add return the sum of the int in the string
-	public void shouldReturnTheSumOfIntInString() {
+	// Test if the method add return the sum of the 2 int in the string
+	public void shouldReturnTheSumOfTwoIntInString() {
 		final int actual = StringCalculator.add("1,2");
 		Assert.assertEquals(3, actual);
 	}
 
+	@Test
+	// Test if the method add return the sum of the int in the string
+	public void shouldReturnTheSumOfIntInString() {
+		final int actual = StringCalculator.add("1,2,3");
+		Assert.assertEquals(6, actual);
+	}
 }
