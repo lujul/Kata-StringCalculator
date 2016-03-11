14290927758d93f6021b183d54db6092b223f15f
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-03-11 10:48:59 +0100

    Manage: custom separator in the string

--- src/main/java/com/humanbooster/formation/testProject/StringCalculator.java
+++ src/main/java/com/humanbooster/formation/testProject/StringCalculator.java
@@ -1,28 +1,48 @@
 package com.humanbooster.formation.testProject;
 
 public class StringCalculator {
-	
+
 	/**
 	 * 
-	 * @param string, string containing integers to be summed
+	 * @param string,
+	 *            string containing integers to be summed
 	 * @return
 	 */
-	public static int add (String string) {
+	public static int add(String string) {
+		int sum = 0;
 		// return 0 if the string is empty
-		if(string.isEmpty()){
-			return 0;
-		}else if (!string.contains(",")){
-			// return the single integer of the string
-			return Integer.valueOf(string);
-		}else {
-			// return the sum of the N intergers in the string
-			String[] valuesInString = string.split(",");
-			int sum=0;
-			for(String nombre: valuesInString)
-				sum+=Integer.valueOf(nombre);
-			return sum; 
-		}
+		if (!string.isEmpty()) {
+			// set the default separators
+			String separator = ",|\n";
 			
+			// set the custom separator if given
+			if (string.startsWith("//")) {
+				separator = separatorSelector(string);
+				
+				// remove the separator option from the string
+				string = string.substring(string.indexOf('\n')+1);
+			}
+			
+			// sum the int values in the string
+			sum = sum(string, separator);
+		}
+		return sum;
 	}
 
+	private static int sum(String string, String separator) {
+		// split the string with the separator
+		String[] valuesInString = string.split(separator);
+		
+		
+		int sum = 0;
+		for (String nombre : valuesInString) {
+			sum += Integer.valueOf(nombre);
+		}
+		return sum;
+	}
+
+	private static String separatorSelector(String string) {
+		String[] valuesInString = string.split("//|\n");
+		return valuesInString[1];
+	}
 }
--- src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
+++ src/test/java/com/humanbooster/formation/testProject/StringCalculatorTest.java
@@ -33,17 +33,38 @@ public class StringCalculatorTest {
 	}
 
 	@Test
-	// Test if the method add return the sum of the int in the string
-	public void shouldReturnTheSumOfIntInString() {
+	// Test if the method add return the sum of the two int in the string
+	public void shouldReturnTheSumOfTwoIntInString() {
 		final int actual = StringCalculator.add("1,2");
 		Assert.assertEquals(3, actual);
 	}
 
 	@Test
 	// Test if the method add return the sum of the int in the string
-	public void acceptePleinDeParametres() {
+	public void shouldReturnTheSumOfIntInString() {
 		final int actual = StringCalculator.add("1,2,3");
 		Assert.assertEquals(6, actual);
 	}
+	
+	@Test
+	// Test if the method add return the sum of the int in the string
+	public void shouldReturnTheSumOfIntSeparatedByComasOrNewLineInString() {
+		int actual = StringCalculator.add("1,2\n3");
+		Assert.assertEquals(6, actual);
+		actual = StringCalculator.add("1,2,3\n25,4\n7");
+		Assert.assertEquals(42, actual);
+		actual = StringCalculator.add("1,2\n5");
+		Assert.assertEquals(8, actual);
+	}
 
+	@Test
+	// Test if the method add return the sum of the int in the string
+	public void shouldReturnTheSumOfIntSeparatedDelimiterInString() {
+		int actual = StringCalculator.add("//;\n1;3");
+		Assert.assertEquals(4, actual);
+		actual = StringCalculator.add("//toto\n1toto3toto22");
+		Assert.assertEquals(26, actual);
+		actual = StringCalculator.add("//	\n1	3	7");
+		Assert.assertEquals(11, actual);
+	}
 }
